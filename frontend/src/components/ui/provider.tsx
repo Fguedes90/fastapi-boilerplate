69b0653ef88cba6f86569861e7c26ce0fe020e15
Francis Guedes Goncalves <fguedes13@gmail.com>
2025-02-18 10:23:00 -0300

    feat: add ShadCN/UI components and utilities for improved styling and layout

--- frontend/src/components/ui/provider.tsx
+++ frontend/src/components/ui/provider.tsx
@@ -1,18 +1,20 @@
-"use client"
-
 import { ChakraProvider } from "@chakra-ui/react"
 import React, { type PropsWithChildren } from "react"
 import { system } from "../../theme"
 import { ColorModeProvider } from "./color-mode"
 import { Toaster } from "./toaster"
+import { ThemeProvider } from "./theme-provider"
+import "../../globals.css"
 
 export function CustomProvider(props: PropsWithChildren) {
   return (
-    <ChakraProvider value={system}>
-      <ColorModeProvider defaultTheme="light">
-        {props.children}
-      </ColorModeProvider>
-      <Toaster />
-    </ChakraProvider>
+    <ThemeProvider defaultTheme="light">
+      <ChakraProvider value={system}>
+        <ColorModeProvider defaultTheme="light">
+          {props.children}
+        </ColorModeProvider>
+        <Toaster />
+      </ChakraProvider>
+    </ThemeProvider>
   )
 }
